07a0629112e6f92f470dc4703b579e67f5b4678a
Toño <salvatuidea@gmail.com>
2020-11-19 18:05:38 +0100

    add notes-app

---
+++ staff/antonio-izquierdo/notes-app/src/logic/index.js
@@ -0,0 +1,8 @@
+import registerUser from './register-user'
+import authenticateUser from './authenticate-user'
+import retrieveUser from './retrieve-user'
+export {
+    registerUser,
+    authenticateUser,
+    retrieveUser
+}
